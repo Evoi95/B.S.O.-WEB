9319199f0f103d660521f49f8ed45572fedc68de
Evoi95 <52084641+Evoi95@users.noreply.github.com>
2020-11-03 20:24:58 +0100

    perchè sono simpy

--- src/database/Main.java
+++ src/database/Main.java
@@ -8,9 +8,12 @@ public class Main {
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		
+		System.out.print("Sono bravo a rompere le cose infomartiche");
+		
+		if (false) {
 		ConnToDb.InitailConnection();
 		ConnToDb.connection();
-		
+		}
 	  }
 
 }
